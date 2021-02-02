a7159ab6e45822be2920066b7b867a74b367f8ce
Tomas Pramuka <tomasp2@kentico.com>
2021-02-02 14:20:17 +0100

    Add Bynder Image Selector form control files with CI xml file

---
+++ src/CMS/CMSFormControls/Kentico.Xperience.Bynder/BynderImageSelector.ascx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using CMS.FormEngine.Web.UI;
+
+
+/// <summary>
+/// Form component for selecting a Bynder image.
+/// Supports single select mode only.
+/// </summary>
+public partial class CMSFormControls_BynderImageSelector : FormEngineUserControl
+{
+    /// <summary>
+    /// Gets or sets the URL of the selected image.
+    /// </summary>
+    public override object Value
+    {
+        get
+        {
+            return imageUrl.Value;
+        }
+        set
+        {
+            imageUrl.Value = value as string;
+        }
+    }
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+    protected override void OnPreRender(EventArgs e)
+    {
+        base.OnPreRender(e);
+    }
+}

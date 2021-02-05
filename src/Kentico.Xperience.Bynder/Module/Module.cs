5d62e06fe163e6b51055a7e37dd351b33f1b2b2c
Tomas Pramuka <tomasp2@kentico.com>
2021-02-05 10:52:12 +0100

    Add Kentico.Xperience.Bynder module project

---
+++ src/Kentico.Xperience.Bynder/Module/Module.cs
@@ -0,0 +1,30 @@
+﻿using CMS;
+
+[assembly: AssemblyDiscoverable]
+
+[assembly: RegisterModule(typeof(Kentico.Xperience.Bynder.Module))]
+
+namespace Kentico.Xperience.Bynder
+{
+    /// <summary>
+    /// Represents the Bynder image selector module for the Xperience admin interface.
+    /// </summary>
+    public class Module : CMS.DataEngine.Module
+    {
+        public const string NAME = "Kentico.Xperience.Bynder";
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Module"/> class.
+        /// </summary>
+        public Module() : base(NAME)
+        {
+        }
+
+
+        protected override void OnInit()
+        {
+            base.OnInit();
+        }
+    }
+}
